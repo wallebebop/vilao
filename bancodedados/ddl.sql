d9fb1d54e9ccae578337aa57fce47da5f6eaf291
wallebebop <diogooomed777@gmail.com>
2022-06-07 18:18:00 -0300

    iauier

---
+++ bancodedados/ddl.sql
@@ -0,0 +1,9 @@
+create database tbvilao;
+use tbvilao;
+	
+create table tb_vilao(
+id_vilao		 int primary key auto_increment,
+nm_vilao  		 varchar(200),
+ds_malvadezas    varchar(200),
+ds_super_poder   boolean);
+
---
+++ bancodedados/dml.sql
@@ -0,0 +1,12 @@
+insert into tb_vilao(
+nm_vilao, 		 
+ds_malvadezas,    
+bt_superpoder     
+)
+
+
+values ( 'thanos', 'aniquilação' , true);
+
+select * from tb_vilao;
+
+
